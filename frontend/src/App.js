dccb439629c47d307279b85ee2fcc9633ae75244
Bankot <fkwasik@interia.pl>
2022-07-01 20:27:59 +0200

    added router to react App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import Form from "./Components/Form/Form"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./app.css"
 function App() {
 	return (
-		<div className='main-container'>
-			<Form></Form>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/login' element={<Form />} />
+				<Route path='/register' element={<Form register='true' />} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
