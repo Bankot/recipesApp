b225efe19ab48744c9903ce5c8d865c0cc1339c0
Bankot <fkwasik@interia.pl>
2022-07-01 21:55:14 +0200

    added navbar and JWT handling Log in and log out mechanics, we are in good shape!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Form from "./Components/Form/Form"
+import Navbar from "./Components/Navbar/Navbar"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./app.css"
 function App() {
 	return (
 		<BrowserRouter>
+			<Navbar></Navbar>
 			<Routes>
 				<Route path='/login' element={<Form />} />
 				<Route path='/register' element={<Form register='true' />} />
---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react"
+import jwt_decode from "jwt-decode"
+import { Link } from "react-router-dom"
+import "./Navbar.css"
+const Navbar = () => {
+	const [userId, setUserId] = useState(null)
+	const [login, setLogin] = useState(null)
+
+	useEffect(() => {
+		const data = window.localStorage.getItem("authorization")
+		if (data) {
+			let token = data.slice(7)
+			let { _id, login } = jwt_decode(token)
+
+			if (_id && login) {
+				setUserId(_id)
+				setLogin(login)
+			}
+		}
+	}, [login])
+	const handleLogOut = () => {
+		window.localStorage.setItem("authorization", "")
+		window.location.href = "/"
+	}
+	return (
+		<ul className='navbar-ul'>
+			<a className='navbar-item'>Home</a>
+			{window.localStorage.getItem("authorization") ? (
+				<>
+					<Link className='navbar-item' to={`/user/${userId}`}>
+						{login}
+					</Link>
+					<a className='navbar-item' onClick={handleLogOut}>
+						Log Out
+					</a>
+				</>
+			) : (
+				<>
+					<Link className='navbar-item' to='/login'>
+						Log In
+					</Link>
+					<Link className='navbar-item' to='/register'>
+						Register
+					</Link>
+				</>
+			)}
+		</ul>
+	)
+}
+
+export default Navbar
